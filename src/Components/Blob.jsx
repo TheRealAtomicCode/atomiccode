e0d291bf63abfefd94f9ac20f6ffc5faff9527c6
Abd al Qader Baghi <aqbaghi@atomiccode.uk>
2023-02-26 00:51:33 +0000

    ade the blob follow the mouse after scrolling

--- src/Components/Blob.jsx
+++ src/Components/Blob.jsx
@@ -1,40 +1,36 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Blob() {
-    const blobRef = useRef(null);
-    const [position, setPosition] = useState({ x: 180, y: 190 });
+    const [position, setPosition] = useState({ x: 180, y: 290 });
 
     useEffect(() => {
         const handleMouseMove = (event) => {
             const x = event.clientX;
-            const y = event.clientY;
-            const { left, top } = blobRef.current.getBoundingClientRect();
-            const dx = x - (left + 100);
-            const dy = y - (top + 100);
-            setPosition({ x: x - dx / 2, y: y - dy / 2 });
+            const y = event.pageY;
+            setPosition({ x, y });
+        };
+
+        const handleScroll = () => {
+            setPosition((prevState) => ({
+                ...prevState,
+                y: window.pageYOffset + 290,
+            }));
         };
 
         window.addEventListener('mousemove', handleMouseMove);
+        window.addEventListener('scroll', handleScroll);
 
         return () => {
             window.removeEventListener('mousemove', handleMouseMove);
         };
     }, []);
 
-    // setInterval(() => {
-    //     setRotate(rotate + 10);
-    //     if (rotate === 360) {
-    //         setRotate(0);
-    //     }
-    // }, 50);
-
     return (
         <div
             id="blob"
-            ref={blobRef}
-            className="fixed bg-gradient-to-r blur-3xl from-blue-400 to-purple-500 w-96 h-72 rounded-full animate-rotate-custom"
+            className="absolute bg-gradient-to-r blur-3xl from-blue-400 to-purple-500 w-96 h-72 rounded-full animate-rotate-custom"
             style={{
-                transform: `translate(${position.x - 90}px, ${position.y - 90}px)  scale(1.2)`, // edit the scale() value to adjust the morphing
+                transform: `translate(${position.x - 120}px, ${position.y - 190}px)  scale(1.2)`, // edit the scale() value to adjust the morphing
                 transition: 'transform 0.8s ease-out',
             }}
         ></div>
