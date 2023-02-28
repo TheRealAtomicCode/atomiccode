894eba0c262b3cf46fcfa8d39afb64f4dbc7a663
Abd al Qader Baghi <aqbaghi@atomiccode.uk>
2023-02-28 20:35:48 +0000

    added the projects into a component

---
+++ src/Components/Projects.jsx
@@ -0,0 +1,124 @@
+const Projects = () => {
+    return (
+        <div className="relative text-white">
+            <h2 className="text-center text-3xl">Projects</h2>
+            <div className="flex flex-wrap justify-center py-8 px-4 sm:px-8 lg:px-16">
+                <div className="w-full sm:w-1/2 lg:w-1/4 p-4">
+                    <div className="bg-gray-800 h-full rounded-lg overflow-hidden shadow-lg">
+                        <div className="bg-gray-500 h-48 md:h-64"></div>
+                        <div className="p-4">
+                            <h3 className="text-xl font-medium mb-2">Atomic.dev</h3>
+                            <p className="text-gray-400 text-base mb-4">
+                                A platform for managing your cloud infrastructure
+                            </p>
+                            <div className="flex justify-between">
+                                <a
+                                    href="https://github.com/user/repo"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="text-gray-400 hover:text-white"
+                                >
+                                    GitHub Repo
+                                </a>
+                                <a
+                                    href="https://example.com"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="text-gray-400 hover:text-white"
+                                >
+                                    Website
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-full sm:w-1/2 lg:w-1/4 p-4">
+                    <div className="bg-gray-800 h-full rounded-lg overflow-hidden shadow-lg">
+                        <div className="bg-gray-500 h-48 md:h-64"></div>
+                        <div className="p-4">
+                            <h3 className="text-xl font-medium mb-2">NASA API app</h3>
+                            <p className="text-gray-400 text-base mb-4">
+                                An application that displays information from NASA's public APIs
+                            </p>
+                            <div className="flex justify-between">
+                                <a
+                                    href="https://github.com/user/repo"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="text-gray-400 hover:text-white"
+                                >
+                                    GitHub Repo
+                                </a>
+                                <a
+                                    href="https://example.com"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="text-gray-400 hover:text-white"
+                                >
+                                    Website
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-full sm:w-1/2 lg:w-1/4 p-4">
+                    <div className="bg-gray-800 h-full rounded-lg overflow-hidden shadow-lg">
+                        <div className="bg-gray-500 h-48 md:h-64"></div>
+                        <div className="p-4">
+                            <h3 className="text-xl font-medium mb-2">Chat-Firebase</h3>
+                            <p className="text-gray-400 text-base mb-4">
+                                A chat application built with Firebase
+                            </p>
+                            <div className="flex justify-between">
+                                <a
+                                    href="https://github.com/user/repo"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="text-gray-400 hover:text-white"
+                                >
+                                    GitHub Repo
+                                </a>
+                                <a
+                                    href="https://example.com"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="text-gray-400 hover:text-white"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-full sm:w-1/2 lg:w-1/4 p-4">
+                    <div className="bg-gray-800 h-full rounded-lg overflow-hidden shadow-lg">
+                        <div className="bg-gray-500 h-48 md:h-64"></div>
+                        <div className="p-4">
+                            <h3 className="text-xl font-medium mb-2">Portfolio Website</h3>
+                            <p className="text-gray-400 text-base mb-4">
+                                My personal portfolio website built with React
+                            </p>
+                            <div className="flex justify-between">
+                                <a
+                                    href="https://github.com/user/repo"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="text-gray-400 hover:text-white"
+                                >
+                                    GitHub Repo
+                                </a>
+                                <a
+                                    href="https://example.com"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="text-gray-400 hover:text-white"
+                                >
+                                    Website
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Projects;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Nav from '../Components/Nav';
 import Blob from '../Components/Blob';
 import Intro from '../Components/Intro';
 import TechStack from '../Components/TechStack';
+import Projects from '../Components/Projects';
 
 import { useState, useRef, useEffect } from 'react';
 
@@ -39,126 +40,7 @@ export default function Home() {
           </ParallaxLayer>
 
           <ParallaxLayer offset={2} factor={1} speed={1}>
-
-            <div className="relative text-white">
-              <h2 className="text-center text-3xl">Projects</h2>
-              <div className="flex flex-wrap justify-center py-8 px-4 sm:px-8 lg:px-16">
-                <div className="w-full sm:w-1/2 lg:w-1/4 p-4">
-                  <div className="bg-gray-800 h-full rounded-lg overflow-hidden shadow-lg">
-                    <div className="bg-gray-500 h-48 md:h-64"></div>
-                    <div className="p-4">
-                      <h3 className="text-xl font-medium mb-2">Atomic.dev</h3>
-                      <p className="text-gray-400 text-base mb-4">
-                        A platform for managing your cloud infrastructure
-                      </p>
-                      <div className="flex justify-between">
-                        <a
-                          href="https://github.com/user/repo"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="text-gray-400 hover:text-white"
-                        >
-                          GitHub Repo
-                        </a>
-                        <a
-                          href="https://example.com"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="text-gray-400 hover:text-white"
-                        >
-                          Website
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="w-full sm:w-1/2 lg:w-1/4 p-4">
-                  <div className="bg-gray-800 h-full rounded-lg overflow-hidden shadow-lg">
-                    <div className="bg-gray-500 h-48 md:h-64"></div>
-                    <div className="p-4">
-                      <h3 className="text-xl font-medium mb-2">NASA API app</h3>
-                      <p className="text-gray-400 text-base mb-4">
-                        An application that displays information from NASA's public APIs
-                      </p>
-                      <div className="flex justify-between">
-                        <a
-                          href="https://github.com/user/repo"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="text-gray-400 hover:text-white"
-                        >
-                          GitHub Repo
-                        </a>
-                        <a
-                          href="https://example.com"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="text-gray-400 hover:text-white"
-                        >
-                          Website
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="w-full sm:w-1/2 lg:w-1/4 p-4">
-                  <div className="bg-gray-800 h-full rounded-lg overflow-hidden shadow-lg">
-                    <div className="bg-gray-500 h-48 md:h-64"></div>
-                    <div className="p-4">
-                      <h3 className="text-xl font-medium mb-2">Chat-Firebase</h3>
-                      <p className="text-gray-400 text-base mb-4">
-                        A chat application built with Firebase
-                      </p>
-                      <div className="flex justify-between">
-                        <a
-                          href="https://github.com/user/repo"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="text-gray-400 hover:text-white"
-                        >
-                          GitHub Repo
-                        </a>
-                        <a
-                          href="https://example.com"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="text-gray-400 hover:text-white"></a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="w-full sm:w-1/2 lg:w-1/4 p-4">
-                  <div className="bg-gray-800 h-full rounded-lg overflow-hidden shadow-lg">
-                    <div className="bg-gray-500 h-48 md:h-64"></div>
-                    <div className="p-4">
-                      <h3 className="text-xl font-medium mb-2">Portfolio Website</h3>
-                      <p className="text-gray-400 text-base mb-4">
-                        My personal portfolio website built with React
-                      </p>
-                      <div className="flex justify-between">
-                        <a
-                          href="https://github.com/user/repo"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="text-gray-400 hover:text-white"
-                        >
-                          GitHub Repo
-                        </a>
-                        <a
-                          href="https://example.com"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="text-gray-400 hover:text-white"
-                        >
-                          Website
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
+            <Projects />
           </ParallaxLayer>
 
           <ParallaxLayer offset={3} factor={1} speed={1}>
